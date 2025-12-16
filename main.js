5497dab23391bfc160a135b4aebe02981237d2ae
Lord William <mvuleniwilliam@gmail.com>
2025-12-16 19:00:07 +0200

    Initial commit of Garden House website

---
+++ main.js
@@ -0,0 +1,10 @@
+import './src/style.css'
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
